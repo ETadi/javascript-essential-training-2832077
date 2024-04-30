6597e7b523f951d3a3cb20852c744d7fa17a293a
ETadi <125534410+ETadi@users.noreply.github.com>
2024-04-30 15:25:32 -0500

    no

--- 04_01/script.js
+++ 04_01/script.js
@@ -16,6 +16,26 @@ const everydayPack = new Backpack(
   "December 5, 2018 15:00:00 PST"
 );
 
+const content = `
+<main>
+    <article>
+      <h1>${everydayPack.name}</h1>
+      <ul>
+        <li>Volume:${everydayPack.name}</li>
+        <li>Color:${everydayPack.name}</li>
+        <li>Age:${everydayPack.name}</li>
+        <li>Number of pockets:${everydayPack.name}</li>
+        <li>Left strap length:${everydayPack.name}</li>
+        <li>Right strap length:${everydayPack.name}</li>
+        <li>Lid status:${everydayPack.name}</li>
+      </ul>
+    </article>
+</main>
+
+`;
+
+document.body.innerHTML = content;
+
 console.log("The everydayPack object:", everydayPack);
 console.log("The pocketNum value:", everydayPack.pocketNum);
 console.log("Days since aquired:", everydayPack.backpackAge());
--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,66 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const bookshelf = {
+  name: "Bookshelf",
+  volume: 100,
+  color: "black",
+  shelfNum: 7,
+  shelflength: 12,
+  shelfFull: false,
+  toggleShelf: function (shelfStatus) {
+    //method
+    this.shelfFull = shelfStatus;
+  },
+};
+
+console.log("The shelf object:", bookshelf);
+console.log("The shelfNum value:", bookshelf.shelfNum);
+
+console.log("Shelf empty before:", bookshelf.shelfFull);
+bookshelf.toggleShelf(true);
+console.log("Shelf empty after:", bookshelf.shelfFull);
+
+const printer = {
+  name: "Printer",
+  type: "color",
+  brand: "HP",
+  paperCapactiy: 100,
+  remainPaper: 100,
+  trayFull: false,
+
+  trayStatus: function (tstatus) {
+    this.trayFull = tstatus;
+  },
+};
+
+console.log("The Printer brand:", printer.brand);
+
+/*
+  const backpack = {
+    name: "Everyday Backpack",
+    volume: 30,
+    color: "grey",
+    pocketNum: 15,
+    strapLength: {
+      left: 26,
+      right: 26,
+    },
+    lidOpen: false,
+    toggleLid: function (lidStatus) {
+      this.lidOpen = lidStatus;
+    },
+    newStrapLength: function (lengthLeft, lengthRight) {
+      this.strapLength.left = lengthLeft;
+      this.strapLength.right = lengthRight;
+    },
+  };
+  
+  console.log("The backpack object:", backpack);
+  console.log("The pocketNum value:", backpack.pocketNum);
+  console.log("Strap Length L:", backpack.strapLength.left);
+  
+  var query = "pocketNum";
+  
+  console.log("The pocketNum value:", backpack[query]); */
